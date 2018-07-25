070fe24e5ec2d05a5d0d0370bdf4d9e9463c3135
Arturo Filio Villa <arturfil@hotmail.com>
2018-07-25 02:57:48 -0400

    added new way of adding images in this case CATEGORIES into the RESTAURANT side of the app

---
+++ app/src/main/java/com/arturofilio/ordereatsserver/Interface/ItemClickListener.java
@@ -0,0 +1,9 @@
+package com.arturofilio.ordereatsserver.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+
+    void onClick(View view,int position, boolean isLongClick);
+
+}
